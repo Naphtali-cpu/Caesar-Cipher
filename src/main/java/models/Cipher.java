c7d4aab5b140313fc6918ab9c299189b6720a56c
Naphtali-cpu <naphtali919@gmail.com>
2021-07-30 19:29:10 +0300

    Add new files

---
+++ src/main/java/models/Cipher.java
@@ -0,0 +1,22 @@
+package models;
+public class Cipher {
+    private int height;
+
+    public Cipher(int length, int width) {
+        height = height;
+    }
+
+    public int getLength() {
+        return height;
+    }
+
+    public int getWidth() {
+        return 0;
+    }
+
+    public boolean isCipher() {
+        int width = 0;
+        return height == width;
+    }
+
+}
